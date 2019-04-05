30cf81a8ad1834eaad5103b413d0d7d9c42f2b9e
ZoomTen <zy.impossible@gmail.com>
2019-04-05 10:45:03 +0000

    fix bad python and print element names

--- parse_ove.py
+++ parse_ove.py
@@ -7,7 +7,7 @@ class OVE:
     media   = []
     sequences=[]
     
-    def __init__():
+    def __init__(self):
         pass
     
     def parsefile(self, filename):
@@ -15,22 +15,23 @@ class OVE:
         project = etree.fromstring(project_string)
         
         for node in project.getchildren():
-        if node.tag == "version":
-            self.version = node.text
-        elif node.tag == "url":
-            self.url = node.text
-        elif node.tag == "folders":
-            for folder in node.getchildren():
-                self.folders.append(folder)
-        elif node.tag == "media":
-            for media in node.getchildren():
-                self.media.append(media)
-        elif node.tag == "sequences":
-            for sequence in node.getchildren():
-                self.sequences.append(sequence)
-        else:
-            pass
+            if node.tag == "version":
+                self.version = node.text
+            elif node.tag == "url":
+                self.url = node.text
+            elif node.tag == "folders":
+                for folder in node.getchildren():
+                    self.folders.append(folder)
+            elif node.tag == "media":
+                for media in node.getchildren():
+                    self.media.append(media)
+            elif node.tag == "sequences":
+                for sequence in node.getchildren():
+                    self.sequences.append(sequence)
+            else:
+                pass
 
+                
 if __name__ == "__main__":
     Project = OVE()
     
@@ -40,3 +41,15 @@ if __name__ == "__main__":
     print("Project bin folders:\n", Project.folders)
     print("Project bin media:\n", Project.media)
     print("Project bin sequences:\n", Project.sequences)
+    
+    print("\n"+"="*20+"FOLDERS"+"="*20)
+    for folder in Project.folders:
+        print(folder.attrib["name"])
+    
+    print("\n"+"="*20+"MEDIA"+"="*20)
+    for media in Project.media:
+        print(media.attrib["name"])
+    
+    print("\n"+"="*20+"SEQUENCE"+"="*20)
+    for sequence in Project.sequences:
+        print(sequence.attrib["name"])
